ab8e4fb55005d176b50d000a7048742fa13ec6cc
Dawid Szoldra <dawid.szoldra17@gmail.com>
2025-12-23 14:51:05 +0100

    Cleanup

--- Source/GameplayShowcase/GameplayShowcase.Build.cs
+++ Source/GameplayShowcase/GameplayShowcase.Build.cs
@@ -31,19 +31,6 @@ public class GameplayShowcase : ModuleRules
 
 		PublicIncludePaths.AddRange(new string[] {
 			"GameplayShowcase",
-			"GameplayShowcase/Variant_Platforming",
-			"GameplayShowcase/Variant_Platforming/Animation",
-			"GameplayShowcase/Variant_Combat",
-			"GameplayShowcase/Variant_Combat/AI",
-			"GameplayShowcase/Variant_Combat/Animation",
-			"GameplayShowcase/Variant_Combat/Gameplay",
-			"GameplayShowcase/Variant_Combat/Interfaces",
-			"GameplayShowcase/Variant_Combat/UI",
-			"GameplayShowcase/Variant_SideScrolling",
-			"GameplayShowcase/Variant_SideScrolling/AI",
-			"GameplayShowcase/Variant_SideScrolling/Gameplay",
-			"GameplayShowcase/Variant_SideScrolling/Interfaces",
-			"GameplayShowcase/Variant_SideScrolling/UI"
 		});
 
 		// Uncomment if you are using Slate UI
